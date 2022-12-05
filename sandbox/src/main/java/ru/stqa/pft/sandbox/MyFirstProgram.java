ce3c5da43f589f4a7bc455f9cbe9c75798c7174e
Dmitry-K1 <117998260+Dmitry-K1@users.noreply.github.com>
2022-12-05 20:45:57 +0300

    Программ расстояние между точкам и метом Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -15,7 +15,13 @@ public class MyFirstProgram {
 
 		System.out.println("Площадь прямоугольника со сторонами "+r.a+" и "+r.b+" = "+r.area());
 
+		Point p=new Point(4,6,8,10);
+
+		System.out.println("Расстояние между точками (" + p.toString() + ") и (" + p.toString1() + ") = " + p.distance());
 	}
+
+
+
 	public static void hello(String somebody){
 		System.out.println ("Hello, "+somebody+"!!");
 	}
